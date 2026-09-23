b794ad0ae3fedfe3876731647ededa558d353a58
Tiger Agent <tigeragentt@gmail.com>
2026-09-22 22:20:02 -0300

    feat: add COM100 - Pensamento Computacional discipline (empty)

--- src/disciplines.js
+++ src/disciplines.js
@@ -1,5 +1,6 @@
 import let110 from './questions/let110.json';
 import soc100 from './questions/soc100.json';
+import com100 from './questions/com100.json';
 
 const disciplines = [
   {
@@ -11,6 +12,11 @@ const disciplines = [
     code: 'SOC100',
     color: '#0891b2',
     data: soc100,
+  },
+  {
+    code: 'COM100',
+    color: '#059669',
+    data: com100,
   },
 ];
 
